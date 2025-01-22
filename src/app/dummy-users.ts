681628fdfc4b87520c903ddb9157ceaeb763484d
Abdallha <126670745+AbdullahJava@users.noreply.github.com>
2025-01-23 00:29:11 +0200

    Section 2

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'zynab Ashraf',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Tasnem Alaa',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Ali Mohamed',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Ahmed Khaled',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Menna Hassan',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Abdallha Hamdy',
+    avatar: 'user-6.jpg',
+  },
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    standalone: false,
-    templateUrl: './header.component.html'
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
